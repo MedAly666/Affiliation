577266637f3f1e1b779372c366d568d47b4ae496
medaly666 <medaly700222@gmail.com>
2025-07-01 23:11:07 +0100

    feat: enhance header component with NavHamburger integration and conditional rendering

--- ali-express-explorer/src/lib/components/Header.svelte
+++ ali-express-explorer/src/lib/components/Header.svelte
@@ -6,6 +6,7 @@
 		NavBrand,
 		ButtonGroup,
 		DarkMode,
+		NavHamburger,
 	} from "flowbite-svelte";
 	import logo from "$lib/assets/logo.png";
 	import { SearchOutline } from "flowbite-svelte-icons";
@@ -19,30 +20,41 @@
 <Navbar
 	class="fixed start-0 top-0 z-20 bg-primary-100 dark:bg-primary-700 flex"
 >
-	<NavBrand href="/">
-		<img src={logo} class="h-10 sm:h-9 rounded-2xl mr-2" alt="Deals Hunter Logo" />
-		<span
-			class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-			>Deals Hunter</span
-		>
-	</NavBrand>
-	<ButtonGroup>
-		<Input
-			class="w-96 border focus:outline-none"
-			placeholder="Search for products..."
-			bind:value={searchQuery}
-			oninput={(e) =>
-				onSearch(
-					(e.target && (e.target as HTMLInputElement).value) ?? "",
-				)}
+	{#snippet children({ hidden, toggle, NavContainer })}
+		<NavHamburger class="dark:text-white" />
+		<NavBrand href="/">
+			<img
+				src={logo}
+				class="h-10 sm:h-9 rounded-2xl mr-2"
+				alt="Deals Hunter Logo"
+			/>
+			<span
+				class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+				>Deals Hunter</span
+			>
+		</NavBrand>
+		{#if !hidden}
+		<ButtonGroup>
+			<Input
+				class="w-96 border focus:outline-none"
+				placeholder="Search for products..."
+				bind:value={searchQuery}
+				oninput={(e) =>
+					onSearch(
+						(e.target && (e.target as HTMLInputElement).value) ??
+							"",
+					)}
+			/>
+			<Button>
+				<SearchOutline />
+			</Button>
+		</ButtonGroup>
+		{/if}
+		<DarkMode
+			class="text-primary-500 dark:text-primary-600 border dark:bg-gray-700"
 		/>
-		<Button>
-			<SearchOutline />
-		</Button>
-	</ButtonGroup>
-	<DarkMode class="text-primary-500 dark:text-primary-600 border dark:bg-gray-700" />
 
-	<!--div class="flex items-center md:order-2">
+		<!--div class="flex items-center md:order-2">
 		<Avatar id="avatar-menu" src="/images/profile-picture-3.webp" />
 		<NavHamburger />
 	</div>
@@ -55,4 +67,5 @@
 		</DropdownHeader>
 		<DropdownHeader>Sign out</DropdownHeader>
 	</Dropdown-->
+	{/snippet}
 </Navbar>
